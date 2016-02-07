4e10f9020fc1b190c0b2eba2df2a29fe0e4376df
Ashima Jain <ashimajain2595@gmail.com>
2016-02-07 14:16:34 +0530

    First commit

---
+++ plot1.R
@@ -0,0 +1,5 @@
+dat<- read.table("household_power_consumption.txt", header=TRUE, sep = ";")
+data<- subset(dat, Date=="1/2/2007" | Date=="2/2/2007")
+hist(as.numeric(as.character(data$Global_active_power)), col="red", main="Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+dev.copy(png, "plot1.png")
+dev.off()
